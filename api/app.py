a928a12676680f79a763c9afd3ddb2da4262beed
marcospsviana <marcospsviana@users.noreply.github.com>
2021-09-25 16:51:07 +0000

    ✅ Ready to clone and code.

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 """
-project_name base module.
+api base module.
 """
 from fastapi import FastAPI
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,11 @@ from typer.testing import CliRunner
 
 # This next line ensures tests uses its own database and settings environment
 os.environ["FORCE_ENV_FOR_DYNACONF"] = "testing"  # noqa
-# WARNING: Ensure imports from `project_name` comes after this line
+# WARNING: Ensure imports from `api` comes after this line
 
-from project_name.app import app
-from project_name.cli import cli
-from project_name.config import settings
+from api.app import app
+from api.cli import cli
+from api.config import settings
 
 
 @pytest.fixture(autouse=True)
